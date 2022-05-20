d184ed5387ecb25db0043559c7a675242bd6ced5
Dave Ridealgh <davecodingstuff@gmail.com>
2022-05-20 05:32:43 +0100

    add example unit tests

---
+++ __tests__/utils.test.js
@@ -0,0 +1,8 @@
+describe('example unit tests', () => {
+  test('1 + 1 should be 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+  test('9 * 3 should be 27', () => {
+    expect(9 * 3).toBe(27);
+  });
+});
