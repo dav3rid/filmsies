e411aa544ec5c947feb74bd8b30175518af9b973
Dave Ridealgh <davecodingstuff@gmail.com>
2022-05-20 10:30:05 +0100

    add '/' endpoint

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const { getFilms } = require("./controllers/films.controllers");
-const { getDirectorById } = require("./controllers/directors.controllers");
+const { getFilms } = require('./controllers/films.controllers');
+const { getDirectorById } = require('./controllers/directors.controllers');
 const {
-	handlePSQLErrors,
-	handleCustomErrors,
-	handleInternalServerError,
-} = require("./controllers/errors.controllers");
+  handlePSQLErrors,
+  handleCustomErrors,
+  handleInternalServerError,
+} = require('./controllers/errors.controllers');
 
 app.use(express.json());
 
-app.get("/api", (req, res, next) => {
-	res.send({ msg: "api up and running" });
+app.get('/', (req, res, next) => {
+  res.send({ msg: 'Hello! I was added via the CICD pipeline 😀' });
 });
 
-app.get("/api/films", getFilms);
-app.get("/api/directors/:director_id", getDirectorById);
+app.get('/api', (req, res, next) => {
+  res.send({ msg: 'api up and running' });
+});
+
+app.get('/api/films', getFilms);
+app.get('/api/directors/:director_id', getDirectorById);
 
 app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
